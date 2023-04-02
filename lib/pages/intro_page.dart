0d179dcd5e404a133437716a459fbb4fa6d1afd3
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2023-04-01 22:04:57 -0500

    Add intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  height: 240,
+                ),
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              // title
+              const Text(
+                'DiCreem',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+
+              const SizedBox(
+                height: 12,
+              ),
+
+              // subtitle
+              const Text(
+                "De Colombia Pa'l mundo",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              // start now button
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12)),
+                padding: const EdgeInsets.all(24),
+                child: const Center(
+                  child: Text(
+                    'Shop now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
